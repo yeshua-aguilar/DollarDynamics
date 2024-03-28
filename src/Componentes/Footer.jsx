56d51d78db5d76f80c187bd2ebf95eef451cb650
yeshuaaguilar <yeshunator.12@gmail.com>
2024-03-28 17:31:44 -0500

    footer terminado

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Avatar } from "@nextui-org/react";
 
 const NewPage = () => {
     return (
@@ -6,9 +7,14 @@ const NewPage = () => {
             <div>
                 <p>20224 - DolarDynamics</p>
 
-
-
-
+                <div className='flex justify-end items-center space-x-4'>
+                    <a href="https://github.com/yeshua-aguilar" target="_blank" rel="noopener noreferrer">
+                        <Avatar src="https://cdn-icons-png.flaticon.com/512/25/25231.png" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/yeshua-aguilar-b2b8721b6/" target="_blank" rel="noopener noreferrer">
+                        <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" />
+                    </a>
+                </div>
 
             </div>
         </div>
